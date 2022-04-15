3015c3fb1ad937dd5828ef5a34456ba9e1636d89
Bernardo Lima <bernardomonclaro@gmail.com>
2022-04-15 00:15:40 -0300

    posts

---
+++ spec/factories/posts.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :post do
+    body { "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Sed tempor placerat nibh, in tristique massa. Curabitur tempus nisl et lacinia molestie. Ut volutpat tincidunt aliquet.
+       Sed massa nisi, sodales at euismod sit amet, luctus eu enim. Etiam sed tristique ante, nec tincidunt lacus.
+       Maecenas vel efficitur felis. Aenean semper mauris non purus facilisis tincidunt.
+       Curabitur euismod, ex sit amet rhoncus suscipit, velit nunc euismod lectus, in condimentum enim lacus sed mi. Etiam lacus dui, mollis vitae risus in, eleifend pretium velit.
+       Nullam eu dolor pulvinar, posuere odio vitae, viverra neque. Vestibulum dictum odio augue, et viverra orci convallis eu."
+      }
+  end
+end
