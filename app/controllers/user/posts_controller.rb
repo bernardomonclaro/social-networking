5f6482e6a940953ffb577140326cc6b69cb43318
Bernardo Lima <bernardomonclaro@gmail.com>
2022-04-25 15:42:12 -0300

    like/unlike posts

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,11 +1,16 @@
 class User::PostsController < UserController
+  skip_before_action :verify_authenticity_token, only: :like_toggle
+
   def create
     f_params = form_params.merge(user: current_user)
+
     @post = Post.new(f_params)
+    @comment = Comment.new
+
     if @post.save
       redirect_to user_profile_path
     else
-      render 'user/profile/show'
+      render "user/profile/show"
     end
   end
 
@@ -14,6 +19,19 @@ class User::PostsController < UserController
     post.destroy
 
     redirect_to user_profile_path
+  end
+
+  def like_toggle
+    post = Post.find(params[:id])
+    like = Like.find_by(user: current_user, post: post)
+
+    if like.present?
+      like.destroy
+      respond_to {|format| format.json { render json: {liked: false, likesCount: post.likes.count}}}
+    else
+      Like.create(user: current_user, post: post)
+      respond_to {|format| format.json { render json: {liked: true, likesCount: post.likes.count}}}
+    end
   end
 
   private
---
+++ spec/models/like_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  it 'user likes a post' do
+    bob = FactoryBot.create(:user)
+    jack = FactoryBot.create(:user)
+    ryan = FactoryBot.create(:user)
+
+    post = FactoryBot.create(:post, user: bob)
+
+    Like.create(user: jack, post: post)
+    expect(post.likes.count).to eq 1
+    expect(post.likes.first.user).to eq jack
+
+    Like.create(user: ryan, post: post)
+    expect(post.likes.count).to eq 2
+    expect(post.likes.last.user).to eq ryan
+
+    Like.find_by(post: post, user: jack).destroy
+    expect(post.likes.count).to eq 1
+    expect(post.likes.last.user).to eq ryan
+
+  end
+end
